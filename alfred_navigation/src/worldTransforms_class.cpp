68ccae9ea7f5201eae58460b579e49f00d1e3451
Balram Kandoria <balramkandoria@gmail.com>
2025-04-06 22:53:02 -0500

    Creating a struct and class to hold and publish static transform data

---
+++ alfred_navigation/src/worldTransforms_class.cpp
@@ -0,0 +1,15 @@
+// Project Alfred Libraries
+#include "worldTransforms_class.h"
+
+// Built-in Cpp Libraries
+#include <string>
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+// ROS2 Libraries
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+#include "sensor_msgs/msg/image.hpp"
+
+
---
+++ alfred_navigation/src/worldTransforms_class.h
@@ -0,0 +1,23 @@
+// Built-in Cpp Libraries
+#include <string>
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+// ROS2 Libraries
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+#include "sensor_msgs/msg/image.hpp"
+
+// class worldTransforms
+// {
+//     public:
+//         // Constructor  
+//         worldTransforms(
+//             rclcpp::Node::SharedPtr transform_node
+//         );
+
+//         // Methods
+
+//     private:
+// }
